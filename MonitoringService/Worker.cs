e12216f2c10a9cf2fca46abae1cbee4fea2cf209
Laura Hartnett <l00179653@atu.ie>
2024-05-13 20:47:30 +0100

    Feat: Add check folder base functionality

--- MonitoringService/Worker.cs
+++ MonitoringService/Worker.cs
@@ -3,18 +3,32 @@ namespace MonitoringService
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private readonly string _folderPath;
 
         public Worker(ILogger<Worker> logger)
         {
             _logger = logger;
+            _folderPath = @"C:\Users\lhartnet\OneDrive - Analog Devices, Inc\Documents\Contemporary Software Development";
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
+                await CheckFolderContents();
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
+                await Task.Delay(10000, stoppingToken);
+            }
+        }
+
+        private async Task CheckFolderContents()
+        {
+            string[] files = Directory.GetFiles(_folderPath);
+
+            _logger.LogInformation($"Files in folder {_folderPath}:");
+            foreach (string file in files)
+            {
+                _logger.LogInformation($"- {Path.GetFileName(file)}");
             }
         }
     }
