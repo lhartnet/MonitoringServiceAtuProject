d462ed2a4fcb216e1bafdbe1cd0556903d865650
Laura Hartnett <l00179653@atu.ie>
2024-05-13 21:33:59 +0100

    Feat: Add try-catch to CheckFolderContents

--- MonitoringService/Worker.cs
+++ MonitoringService/Worker.cs
@@ -3,32 +3,42 @@ namespace MonitoringService
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
-        private readonly string _folderPath;
+        private readonly string _ongoingFolderPath;
+        private readonly string _approvedFolderPath;
 
         public Worker(ILogger<Worker> logger)
         {
             _logger = logger;
-            _folderPath = @"C:\Users\lhartnet\OneDrive - Analog Devices, Inc\Documents\Contemporary Software Development";
+            _ongoingFolderPath = @"C:\Users\lhartnet\OneDrive - Analog Devices, Inc\Documents\Contemporary Software Development";
+            _approvedFolderPath = @"C:\Users\lhartnet\OneDrive - Analog Devices, Inc\Documents\Contemporary Software Development";
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                await CheckFolderContents();
+                await CheckFolderContents(_ongoingFolderPath);
+                await CheckFolderContents(_approvedFolderPath);
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
                 await Task.Delay(10000, stoppingToken);
             }
         }
 
-        private async Task CheckFolderContents()
+        private async Task CheckFolderContents(string filePath)
         {
-            string[] files = Directory.GetFiles(_folderPath);
-
-            _logger.LogInformation($"Files in folder {_folderPath}:");
-            foreach (string file in files)
+            try
             {
-                _logger.LogInformation($"- {Path.GetFileName(file)}");
+                string[] files = Directory.GetFiles(filePath);
+
+                _logger.LogInformation($"Files in folder {filePath}:");
+                foreach (string file in files)
+                {
+                    _logger.LogInformation($"- {Path.GetFileName(file)}");
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error reading folder contents for {filePath}: {ex.Message}");
             }
         }
     }
