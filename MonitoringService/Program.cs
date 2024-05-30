59c9b910ed8b94ed32d747a160d6795d8591c344
Laura Hartnett <l00179653@atu.ie>
2024-05-30 23:35:06 +0100

    Bug fix: Scope of services in program.cs and time between runs

--- MonitoringService/Program.cs
+++ MonitoringService/Program.cs
@@ -1,18 +1,20 @@
 using Microsoft.EntityFrameworkCore;
 using MonitoringService;
-using Microsoft.Extensions.Configuration;
 using MonitoringService.Services;
 
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices((context, services) =>
     {
-        services.Configure<ConfigurableSettings>(context.Configuration.GetSection("FolderPaths"));
+        var connectionString = context.Configuration.GetConnectionString("DefaultConnection");
+        services.AddDbContext<ApplicationContext>(options =>
+            options.UseSqlServer(connectionString));
+
+        services.Configure<ConfigurableSettings>(context.Configuration.GetSection("Options"));
         services.Configure<EmailSettings>(context.Configuration.GetSection("EmailSettings"));
         services.AddSingleton<EmailService>();
         services.AddHostedService<Worker>();
-        services.AddDbContext<ApplicationContext>(options =>
-            options.UseSqlServer(context.Configuration.GetConnectionString("DefaultConnection")));
     })
     .Build();
 
 host.Run();
+
