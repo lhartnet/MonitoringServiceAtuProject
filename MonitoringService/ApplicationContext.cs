c14e37be026b7e635d7ffbca5c68c458018a631e
Laura Hartnett <l00179653@atu.ie>
2024-05-30 22:13:44 +0100

    Add db class

---
+++ MonitoringService/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MonitoringService 
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+        }
+
+        public DbSet<SpecDetails> SpecDetail { get; set; }
+
+    }
+}
